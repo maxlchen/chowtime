b37f902ce8f164f10472020ad1453404c7ee10de
Maximillian Chen <mlc294@cornell.edu>
2018-08-07 00:36:16 -0400

    Remove bad import

--- yelpretrieve.py
+++ yelpretrieve.py
@@ -1,6 +1,6 @@
 #import get_key
 from yelpapi import YelpAPI
-import chooserestaurant
+
 
 #api = get_key.get_key
 
@@ -8,4 +8,3 @@ yelp_api = YelpAPI('V4emNeDA9c8hV2hv1dRCs33iS3QmfQjlpPSvd5LQ8AInkJXwh_VXl-cWAhT6
 
 def getResults(area, category, distance, cost = "1, 2, 3, 4"):
     return yelp_api.search_query(term = "food", categories = category, radius = distance, location = area, price = cost, limit = 50)
-
