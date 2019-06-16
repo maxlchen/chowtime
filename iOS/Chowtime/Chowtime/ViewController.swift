07c18e978ed5a3b02870b620424b21779b6b8279
Max Chen <mlc294@cornell.edu>
2019-06-16 00:49:01 -0400

    Initialize swift project

---
+++ iOS/Chowtime/Chowtime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chowtime
+//
+//  Created by Max Chen on 6/15/19.
+//  Copyright © 2019 intellegoStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
